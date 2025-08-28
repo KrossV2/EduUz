2a6b02c3d82d29e895d6b260bae278d8b6ef9a23
KrossV2 <nasriddinovubaydullo46@gmail.com>
2025-08-28 20:06:17 +0500

    done  director panel

--- EduUz.Application/Mediatr/Directors/Teachers/GetTeacherSubjects/GetTeacherSubjectQuery.cs
+++ EduUz.Application/Mediatr/Directors/Teachers/GetTeacherSubjects/GetTeacherSubjectQuery.cs
@@ -5,18 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EduUz.Application.Mediatr.Teachers.GetTeacherSubjects;
 
-public class GetTeacherSubjectQuery(int id)  : IRequest<List<int>>
+public class GetTeacherSubjectQuery(int id)  : IRequest<List<string>>
 {
     public int Id { get; set; } = id;
 }
 
-public class GetTeacherSubjectQueryHandler(EduUzDbContext context) : IRequestHandler<GetTeacherSubjectQuery, List<int>>
+public class GetTeacherSubjectQueryHandler(EduUzDbContext context) : IRequestHandler<GetTeacherSubjectQuery, List<string>>
 {
-    public async Task<List<int>> Handle(GetTeacherSubjectQuery request, CancellationToken cancellationToken)
+    public async Task<List<string>> Handle(GetTeacherSubjectQuery request, CancellationToken cancellationToken)
     {
         var subjectIds = await context.TeacherSubjects
             .Where(ts => ts.TeacherId == request.Id)
-            .Select(ts => ts.SubjectId)  
+            .Select(ts => ts.Subject.Name)  
             .ToListAsync(cancellationToken);
 
         return subjectIds;
