4fe30206d091c536752b7ff7befba5e4a96a425f
KrossV2 <nasriddinovubaydullo46@gmail.com>
2025-11-15 22:29:27 +0500

    updated database

---
+++ EduUz.Infrastructure/Migrations/20251115172904_AddedSomethingssss.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduUz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSomethingssss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Homeworks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Homeworks_StudentId",
+                table: "Homeworks",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Homeworks_Students_StudentId",
+                table: "Homeworks",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Homeworks_Students_StudentId",
+                table: "Homeworks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Homeworks_StudentId",
+                table: "Homeworks");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Homeworks");
+        }
+    }
+}
